2a4326e9bee441abca04adea013b289361484f29
Benjamin-Dada <benjamin.o.dada@gmail.com>
2015-08-03 13:11:12 +0100

    add new pages(about me, about the site)

---
+++ public/js/app.js
@@ -0,0 +1,15 @@
+var benjamin = angular.module('benjamin', ['ngRoute']);
+
+benjamin.config(['$routeProvider','$locationProvider',function($routeProvider,$locationProvider){
+
+	$routeProvider.when("/",{templateUrl: '/partials/about_me.html', controller: 'mainCtrl'})
+				  .when("/about_site",{templateUrl: '/partials/about_site.html', controller: 'mainCtrl'})
+				  .otherwise({ redirectTo: "/" });
+	$locationProvider.html5Mode(true);
+	}]);
+
+benjamin.controller('mainCtrl',['$scope','$location',function($scope,$location){
+		$scope.isActive = function (viewLocation) { 
+        return viewLocation === $location.path();
+    };
+}]);
