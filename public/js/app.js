37ffb1e345c6d71c7f64923988dc6243efec67bb
Benjamin-Dada <benjamin.o.dada@gmail.com>
2015-08-12 14:39:46 +0100

    Create Side bar, and divide screen into 3

--- public/js/app.js
+++ public/js/app.js
@@ -3,7 +3,7 @@ var benjamin = angular.module('benjamin', ['ngRoute']);
 benjamin.config(['$routeProvider','$locationProvider',function($routeProvider,$locationProvider){
 
 	$routeProvider.when("/",{templateUrl: '/partials/about_me.html', controller: 'mainCtrl'})
-	              .when("/me",{templateUrl: '/partials/me.html', controller: 'mainCtrl'})
+	              .when("/about",{templateUrl: '/partials/about.html', controller: 'mainCtrl'})
 				  .when("/about_site",{templateUrl: '/partials/about_site.html', controller: 'mainCtrl'})
 				  .otherwise({ redirectTo: "/" });
 	$locationProvider.html5Mode(true);
@@ -13,4 +13,6 @@ benjamin.controller('mainCtrl',['$scope','$location',function($scope,$location){
 		$scope.isActive = function (viewLocation) { 
         return viewLocation === $location.path();
     };
+    var today = new Date();
+    $scope.year = today.getFullYear();
 }]);
