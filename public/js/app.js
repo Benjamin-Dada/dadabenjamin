0008dd5d2749f3a08f487e1242a745c0cabaab44
Benjamin-Dada <benjamin.o.dada@gmail.com>
2015-08-14 10:47:45 +0100

    Fix error in Twitter Timeline displaying on the site

--- public/js/app.js
+++ public/js/app.js
@@ -3,16 +3,16 @@ var benjamin = angular.module('benjamin', ['ngRoute']);
 benjamin.config(['$routeProvider','$locationProvider',function($routeProvider,$locationProvider){
 
 	$routeProvider.when("/",{templateUrl: '/partials/about_me.html', controller: 'mainCtrl'})
-	              .when("/about",{templateUrl: '/partials/about.html', controller: 'mainCtrl'})
-				  .when("/about_site",{templateUrl: '/partials/about_site.html', controller: 'mainCtrl'})
-				  .otherwise({ redirectTo: "/" });
+	.when("/about",{templateUrl: '/partials/about.html', controller: 'mainCtrl'})
+	.when("/about_site",{templateUrl: '/partials/about_site.html', controller: 'mainCtrl'})
+	.otherwise({ redirectTo: "/" });
 	$locationProvider.html5Mode(true);
-	}]);
+}]);
 
 benjamin.controller('mainCtrl',['$scope','$location',function($scope,$location){
-		$scope.isActive = function (viewLocation) { 
-        return viewLocation === $location.path();
-    };
-    var today = new Date();
-    $scope.year = today.getFullYear();
+	$scope.isActive = function (viewLocation) { 
+		return viewLocation === $location.path();
+	};
+	var today = new Date();
+	$scope.year = today.getFullYear();
 }]);
