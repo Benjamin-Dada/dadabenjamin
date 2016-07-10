42a69d02b27b46419afb21e64af0014508dfb969
Benjamin Dada <Benjamin Dada>
2016-07-10 12:24:29 +0100

    before I pulled from Online Repo

--- public/js/app.js
+++ public/js/app.js
@@ -3,22 +3,15 @@ benjamin.config(['$routeProvider','$locationProvider',
 	function($routeProvider,$locationProvider)
 	{	$routeProvider
 		.when("/",{templateUrl:'/partials/about_me.html',controller:'mainCtrl'})
+		.when("/gallery",{templateUrl:'/partials/Gallery.html',controller:'mainCtrl'})
 		.when("/projects",{templateUrl:'/partials/projects.html',controller:'mainCtrl'})
-		.when("/gallery",{templateUrl:'/partials/gallery.html',controller:'mainCtrl'})
+		.when("/test",{templateUrl:'/partials/templateId.html',controller:'mainCtrl'})
 		.otherwise({redirectTo:"/"});$locationProvider.html5Mode(true);}
 		]);
 benjamin.controller('mainCtrl',['$scope','$location','$http', '$templateCache',
 	function($scope,$location,$http,$templateCache) 
-	{	$scope.isActive=function(viewLocation)
-		{return viewLocation===$location.path();};
+	{	$scope.isActive=function(viewLocation){return viewLocation===$location.path();};
 		var today=new Date();$scope.year=today.getFullYear();
-		/*$scope.name = "";
-		$scope.email = "";
-		$scope.submit=function(){	
-				var data = {name: $scope.name, email: $scope.email};
-		$http.post('http://localhost:8080/hello', data)
-			.success(function(data){alert("Email Received");})
-			.error(function(data){alert("Error");});
-		}*/
+		$templateCache.put('templateId.html', 'This is the content of the template');
 	}		
 ]);
